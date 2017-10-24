0afda02601ff02c64343fceb130121da1e3352b3
bmiller08 <bmiller@renovo1.com>
2017-10-23 20:14:52 -0400

    Add script

---
+++ route_fixer.ps1
@@ -0,0 +1,11 @@
+﻿$domain = 'example.com'
+$gateway = '192.168.0.1'
+
+foreach($line in [system.net.dns]::resolve($domain).AddressList.IPAddressToString)
+{
+route PRINT | find " $line" > $null
+ if (-NOT $LASTEXITCODE -eq 0)
+ {
+ route ADD $line $gateway METRIC 34
+ }
+}
